84ad8ced1274cf6dfac9e366643b991ad7eba2e3
mamingyang <1624133181@qq.com>
2025-11-28 10:21:52 +0800

    开启sqs

--- gh-gif/src/main/java/com/mawai/ghgif/service/AmazonSQSService.java
+++ gh-gif/src/main/java/com/mawai/ghgif/service/AmazonSQSService.java
@@ -27,7 +27,7 @@ public class AmazonSQSService implements SmartLifecycle {
 
     private final AmazonSQSClientConfig amazonSQSClientConfig;
     private SqsClient sqsClient;
-    private volatile boolean running = true; // 启动前若为true，则sqs关闭
+    private volatile boolean running = false; // 启动前若为true，则sqs关闭
     private final List<MessageConsumer> messageConsumers;
     private List<CustomSQSMessageConsumer> sqsMessageConsumerList = new ArrayList<>();
     
