d0d7cc150e0f5c02e6e05c114d8940b5563ce09d
mamingyang <1624133181@qq.com>
2025-12-08 12:05:59 +0800

    fix-1205

--- gh-admin/src/main/java/com/mawai/ghadmin/dto/GifAuditDTO.java
+++ gh-admin/src/main/java/com/mawai/ghadmin/dto/GifAuditDTO.java
@@ -7,7 +7,6 @@ import java.time.LocalDateTime;
 @Data
 public class GifAuditDTO {
     private Long id;          // 审核ID
-    private Long gifId;       // GIF ID
     private String fileUrl;   // 文件URL
     private String title;
     private String description;
